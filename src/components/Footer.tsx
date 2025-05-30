83fd171bec1065035e2d7408e68931934fd40a0a
kevgilmore <kgilmore.me@gmail.com>
2025-05-30 15:41:51 +0100

    deploy script and footer copy

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 export function Footer() {
   return <footer className="mt-12 py-6 text-center text-gray-600 border-t border-gray-200">
       <p>
-        This is a demo. The real product is coming soon at{' '}
+      We hope you enjoy our demo. The real app will be launching <b>soon</b> to the App Store. <br></br> For more details visit {' '}
         <a href="https://simplysent.co" target="_blank" rel="noopener noreferrer" className="text-purple-600 hover:text-purple-700 font-medium">
           simplysent.co
         </a>
